47ca493ea790178649ae65b64cebe605f5a9edb8
Paqui <paqui.calabria@schibsted.com>
2020-04-20 21:11:52 +0200

    Add search functionality without API call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import SearchBar from './components/SearchBar'
 
-function App() {
-  let searchData = []
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      searchData: [],
+    }
+    this.onNewSearch = this.onNewSearch.bind(this)
+  }
 
-  return (
-    <div className={`App flex justify-center ${ searchData ? 'absolute inset-0 items-center' : 'mt-4'}`} >
-        <SearchBar />
-      </div>
-  );
+  onNewSearch(searchTerm) {
+    console.log('MSG: App -> onNewSearch -> searchTerm', searchTerm)
+    this.setState({searchTerm}, () => {
+      console.log(this.state)
+    })
+  }
+
+  render() {
+    return (
+      <div className={`App flex justify-center ${ this.state.searchData ? 'absolute inset-0 items-center' : 'mt-4'}`} >
+          <SearchBar onSearch={this.onNewSearch}/>
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,38 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-function SearchBar(props) {
-  return (
-    <div>
-      <form className="py-2 px-4 rounded-full bg-white shadow-lg">
-        <input type="text" placeholder="Busca un libro" className="bg-transparent"></input>
-        <button type="submit" aria-label="Buscar" className="text-pink-700">
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </form>
-    </div>
-  )
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchValue: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({searchValue: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.onSearch(this.state.searchValue)
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="py-2 px-4 rounded-full bg-white shadow-lg" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Busca un libro"
+            value={this.state.searchValue}
+            onChange={this.handleChange}
+            className="bg-transparent"></input>
+          <button type="submit" aria-label="Buscar" className="text-pink-700">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SearchBar
