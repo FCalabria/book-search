c980f674a49127bfc3c0feb97e72152340534c53
Paqui <paqui.calabria@schibsted.com>
2020-05-23 18:41:26 +0200

    tests: Visual tests

---
+++ percy.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'version': 1,
+  'snapshot': {
+    'widths': [360, 768, 1024]
+  }
+};
--- tests/e2e/suites/searchBook.js
+++ tests/e2e/suites/searchBook.js
@@ -4,11 +4,17 @@ module.exports = {
   beforeEach: browser => {
     searchPage = browser.page.search()
   },
+
+  afterEach: browser => {
+    // Calling .end right before percySnapshot causes buggy behaviour on percy
+    browser.pause(10)
+    browser.end()
+  },
   'It should have an input and search button': () => {
     searchPage.navigate()
       .assert.visible('@searchInput')
       .assert.visible('@searchButton')
-      .end()
+      .percySnapshot('Home screen')
   },
   'It should show the list of results after a correct search': () => {
     searchPage.searchTerm('Matilda')
@@ -17,7 +23,7 @@ module.exports = {
       .assert.visible('@bookInfo')
       .assert.visible('@similarButton')
       .assert.containsText('@bookInfo', 'Matilda')
-      .end()
+      .percySnapshot('Results screen')
   },
   'It should show an error text for search errors': () => {
     searchPage.searchTerm('ASDasdfasdnsañdfjiaoASDFa')
@@ -26,7 +32,7 @@ module.exports = {
     .expect.element('body').text.to.match(/^no[\w\s]*ASDasdfasdnsañdfjiaoASDFa$/im)
     // Order is important here (wait for error message to appear, then assert that theres no results)
     searchPage.assert.not.elementPresent(searchPage.section.firstResult.selector)
-      .end()
+      .percySnapshot('Error screen')
   },
   'It should search by term when the similarButton is clicked': () => {
     searchPage.searchTerm('Matilda')
@@ -37,6 +43,5 @@ module.exports = {
       .waitForElementVisible('@bookInfo', 10000)
       .assert.visible('@bookInfo')
       .assert.not.containsText('@bookInfo', 'Matilda')
-      .end()
   }
 }
